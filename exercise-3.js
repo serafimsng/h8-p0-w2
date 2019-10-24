3fadc9347c4f9774fcdbf3f286766743c0c7d913
serafimsng <serafimsinaga@gmail.com>
2019-10-24 11:49:17 +0700

    ex

--- exercise-3.js
+++ exercise-3.js
@@ -1,20 +1,20 @@
 var nama = 'Sera';
-var peran = 'Ksatria';
+var peran = 'Tabib';
 
-if(nama === '' && peran === ''){
+if(nama == '' && peran == ''){
 	console.log("Nama dan peran harus diisi!")}
-    else if(nama !== '' && peran !==''){
-	    if (peran === 'Ksatria') {
+    else if(nama != '' && peran !=''){
+	    if (peran == 'Ksatria') {
 		  console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 	  	console.log(`Halo Ksatria ${nama}, kamu dapat menyerang dengan senjatamu!`)
-	    }else if (peran === 'Tabib') {
+	    }else if (peran == 'Tabib') {
 	  	console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 	  	console.log(`Halo Tabib ${nama}, kamu akan membantu temanmu yang terluka.`)
-	    }else if (peran === 'Penyihir') {
+	    }else if (peran == 'Penyihir') {
 	   	console.log(`Selamat datang di Dunia Proxytia, ${nama}`)
 	  	console.log(`Halo Penyihir ${nama}, ciptakan keajaiban yang membantu kemenanganmu!`)
 	}
-}else if (nama !== '' && peran === '') {
+}else if (nama != '' && peran == '') {
 	console.log(`Halo ${nama}, Pilih peranmu untuk memulai game!`)
 }else{
   console.log('Nama Harus Diisi!!')
